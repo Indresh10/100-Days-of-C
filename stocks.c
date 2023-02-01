4a0394c1bbf91d279c7189cdab9e68e17c928858
Indresh Hemani <hemaniindresh@gmail.com>
2023-02-01 23:55:06 +0530

    modified with slicing array concept

--- stocks.c
+++ stocks.c
@@ -2,35 +2,20 @@
 
 int main(int argc, char const *argv[])
 {
-    int n = 5;
-    int arr[] = {7, 5, 2, 1, 1};
-    int minpos = getMinPos(arr, n);
-    int max = getMax(arr, n, minpos);
-    printf("%d\n", max - arr[minpos]);
-    return 0;
-}
-int getMinPos(int *arr, int n)
-{
-    int min = arr[0];
-    int j = 0;
-    for (int i = 1; i < n; i++)
+    int pricesSize = 6;
+    int prices[] = {7, 1, 5, 3, 6, 4};
+    int l = 0;
+    int res = 0;
+    for (int r = 1; r < pricesSize; r++)
     {
-        if (min > arr[i])
+        int cur = prices[r] - prices[l];
+        if (prices[l] < prices[r])
         {
-            min = arr[i];
-            j = i;
+            if (res < cur)
+                res = cur;
         }
+        else
+            l = r;
     }
-    return j;
-}
-
-int getMax(int *arr, int n, int start)
-{
-    int max = arr[start];
-    for (int i = start + 1; i < n; i++)
-    {
-        if (max < arr[i])
-            max = arr[i];
-    }
-    return max;
+    return res;
 }
